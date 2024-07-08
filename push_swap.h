b80f6a34515b2914c99d60a2e11a3edd61141b37
root <sushi.honda@gmail.com>
2024-07-08 21:37:36 +0800

    undefined refer to ft_split: add all functions in .h

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:58:24 by athonda           #+#    #+#             */
-/*   Updated: 2024/07/08 19:22:47 by athonda          ###   ########.fr       */
+/*   Updated: 2024/07/08 20:53:27 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # include <stdlib.h>
 
 typedef	struct s_box t_box;
-struct
+struct s_box
 {
 	int		value;
 	t_box	*previous;
@@ -25,4 +25,8 @@ size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_count_words(char const *str, char delimiter);
+void	ft_cleanmem(unsigned int i, char **word);
+int	ft_setstr(char const *s, char c, size_t nbr_words, char **word);
+char	**ft_split(char const *s, char c);
 #endif // PUSH_SWAP_H
