343dad9e890c0c4ce9e2e34ffa9134e8c2e78d9c
root <sushi.honda@gmail.com>
2024-07-08 19:20:57 +0800

    ft_split and related files

---
+++ ft_utils.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 19:02:19 by athonda           #+#    #+#             */
+/*   Updated: 2024/07/08 19:19:11 by athonda          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+	size_t			i;
+
+	if (!dest && !src && n != 0)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		*(d + i) = *(s + i);
+		i++;
+	}
+	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*ptr;
+	size_t			len_s;
+
+	if (s == NULL)
+		return (NULL);
+	len_s = ft_strlen(s);
+	if (start >= len_s)
+	{
+		ptr = malloc(sizeof (char) * 1);
+		if (ptr == NULL)
+			return (NULL);
+		ptr[0] = '\0';
+		return (ptr);
+	}
+	if (len > len_s - start)
+		len = len_s - start;
+	ptr = (char *)malloc(sizeof (char) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	ft_memcpy(ptr, &s[start], len);
+	ptr[len] = '\0';
+	return (ptr);
+}
--- push_swap.h
+++ push_swap.h
@@ -6,13 +6,14 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:58:24 by athonda           #+#    #+#             */
-/*   Updated: 2024/07/08 18:34:35 by athonda          ###   ########.fr       */
+/*   Updated: 2024/07/08 19:19:56 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include <stdlib.h>
+
 typedef	struct s_box t_box;
 struct
 {
@@ -20,4 +21,8 @@ struct
 	t_box	*previous;
 	t_box	*next;
 };
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char s, int c);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif // PUSH_SWAP_H
