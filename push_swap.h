293328edc0292d3b0c45e28f79e5187c95295481
root <sushi.honda@gmail.com>
2024-07-08 19:23:46 +0800

    revise: typo in prototype declaration

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:58:24 by athonda           #+#    #+#             */
-/*   Updated: 2024/07/08 19:19:56 by athonda          ###   ########.fr       */
+/*   Updated: 2024/07/08 19:22:47 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ struct
 	t_box	*next;
 };
 size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char s, int c);
+char	*ft_strchr(const char *s, int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif // PUSH_SWAP_H
