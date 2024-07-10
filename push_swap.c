2de3c14f40dd20979a8b1354f364456fb82dacba
root <sushi.honda@gmail.com>
2024-07-10 22:16:41 +0800

    rotate

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 18:38:00 by athonda           #+#    #+#             */
-/*   Updated: 2024/07/10 21:48:00 by athonda          ###   ########.fr       */
+/*   Updated: 2024/07/10 22:07:14 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,21 @@ int	main(int argc, char *argv[])
 		}
 		printf("\n");
 		sa(&head_a);
+		now = head_a;
+		if (now != NULL)
+		{
+			i = 1;
+			while (1)
+			{
+				printf("%d: %d @%p next->%p prev->%p\n", i, now->value, now, now->next, now->prev);
+				now = now->next;
+				i++;
+				if (now == head_a)
+					break;
+			}
+		}
+		printf("\n");
+		ra(&head_a);
 		now = head_a;
 		if (now != NULL)
 		{
