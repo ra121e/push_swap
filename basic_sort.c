ad20fdbe0bf4047a047962c41a1d0481779644dc
root <sushi.honda@gmail.com>
2024-07-16 10:58:58 +0800

    comment & small fix: head_a -> head for generalize

--- basic_sort.c
+++ basic_sort.c
@@ -6,10 +6,15 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 09:35:02 by athonda           #+#    #+#             */
-/*   Updated: 2024/07/16 09:45:52 by athonda          ###   ########.fr       */
+/*   Updated: 2024/07/16 10:56:46 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file basic_sort.c
+ * @brief common sort functions
+ * @note maybe bubble should be added in the future?
+ */
 
 #include "push_swap.h"
 
@@ -19,16 +24,16 @@
  * @param[in] head_a is stack a link list
  */
 
-void	two_case(t_box **head_a)
+void	two_case(t_box **head)
 {
 	t_box	*box1;
 	t_box	*box2;
 
-	box1 = *head_a;
+	box1 = *head;
 	box2 = box1->next;
 	if (box2->value < box1->value)
 	{
-		sa(head_a);
+		sa(head);
 	}
 	return;
 }
@@ -38,26 +43,25 @@ void	two_case(t_box **head_a)
  * @brief sort 3 numbers
  * @param[in] head address of linked list
  * @return no
- * @sa two_case
  * @note readable or norm -> I found the way takes both!!
  */
 
-void	three_case(t_box **head_a)
+void	three_case(t_box **head)
 {
 	t_box	*box1;
 	t_box	*box2;
 	t_box	*last_box;
 
-	box1 = *head_a;
+	box1 = *head;
 	box2 = box1->next;
 	last_box = box1->prev;
-	two_case(head_a);
+	two_case(head);
 	if (box1->value > last_box->value)
-		rra(head_a);
+		rra(head);
 	if (box2->value > last_box->value)
 		{
-			ra(head_a);
-			sa(head_a);
-			rra(head_a);
+			ra(head);
+			sa(head);
+			rra(head);
 		}
 }
