6de38364c3eef26a16769c8eb2738a9c63c013e4
root <sushi.honda@gmail.com>
2024-07-18 22:18:56 +0800

    wrong pa pb: swiched

--- algorithm.c
+++ algorithm.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 20:52:11 by athonda           #+#    #+#             */
-/*   Updated: 2024/07/18 18:09:03 by athonda          ###   ########.fr       */
+/*   Updated: 2024/07/18 22:15:04 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_box	*find_target_largest_smaller(t_box *node, t_box **head_to)
 	t_box	*target;
 
 	now = *head_to;
-	def	= 2147483647;
+	def = 2147483647;
 	target = NULL;
 	node_max = ft_lstmax(head_to);
 	while (1)
@@ -35,7 +35,7 @@ t_box	*find_target_largest_smaller(t_box *node, t_box **head_to)
 		{
 			if (target == NULL)
 				target = node_max;
-			return(target);
+			return (target);
 		}
 	}
 }
@@ -48,7 +48,7 @@ t_box	*find_target_smallest_larger(t_box *node, t_box **head_to)
 	t_box	*target;
 
 	now = *head_to;
-	def	= 2147483647;
+	def = 2147483647;
 	target = NULL;
 	node_min = ft_lstmin(head_to);
 	while (1)
@@ -63,7 +63,7 @@ t_box	*find_target_smallest_larger(t_box *node, t_box **head_to)
 		{
 			if (target == NULL)
 				target = node_min;
-			return(target);
+			return (target);
 		}
 	}
 }
@@ -79,8 +79,9 @@ void	push_forward(t_box **head_a, t_box **head_b)
 	{
 		if (*head_b == NULL || (*head_b)->next == NULL)
 		{
-			pa(head_a, head_b);
-			if ((*head_b)->next != NULL && (*head_b)->value < ((*head_b)->next)->value)
+			pb(head_a, head_b);
+			if ((*head_b)->next != NULL &&
+				 (*head_b)->value < ((*head_b)->next)->value)
 				sb(head_b);
 		}
 		else
@@ -89,7 +90,7 @@ void	push_forward(t_box **head_a, t_box **head_b)
 			base = find_base(head_a, head_b);
 			target = find_target_largest_smaller(base, head_b);
 			revolver(head_a, head_b, base, target);
-			pa(head_a, head_b);
+			pb(head_a, head_b);
 			last_box = (*head_a)->prev;
 		}
 	}
@@ -108,11 +109,11 @@ void	push_back(t_box **head_a, t_box **head_b)
 		while (target != *head_a)
 		{
 			if (prevcost < nextcost)
-				rb(head_a);
+				ra(head_a);
 			else
-				rrb(head_a);
+				rra(head_a);
 		}
-		pb(head_b, head_a);
+		pa(head_b, head_a);
 	}
 }
 
--- op_push.c
+++ op_push.c
@@ -6,17 +6,17 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:26:05 by athonda           #+#    #+#             */
-/*   Updated: 2024/07/12 17:53:20 by athonda          ###   ########.fr       */
+/*   Updated: 2024/07/18 22:13:17 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	pa(t_box **head_a, t_box **head_b)
+void	pb(t_box **head_a, t_box **head_b)
 {
 	t_box	*box1;
 
-	write(1, "pa\n", 3);
+	write(1, "pb\n", 3);
 	if (*head_a == NULL)
 		return;
 	box1 = ft_lstcut_front(head_a);
@@ -25,11 +25,11 @@ void	pa(t_box **head_a, t_box **head_b)
 	ft_lstadd_front(head_b, box1);
 }
 
-void	pb(t_box **head_b, t_box **head_a)
+void	pa(t_box **head_b, t_box **head_a)
 {
 	t_box	*box1;
 
-	write(1, "pb\n", 3);
+	write(1, "pa\n", 3);
 	if (*head_b == NULL)
 		return;
 	box1 = ft_lstcut_front(head_b);
