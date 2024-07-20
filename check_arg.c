47258d74541bef32352c5d1e31c1f8ea30283850
root <sushi.honda@gmail.com>
2024-07-20 14:47:20 +0800

    integer MAX MIN no error: in dup check, MAX MIN condition didnt reach in while loop: new function

--- check_arg.c
+++ check_arg.c
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 20:10:32 by athonda           #+#    #+#             */
-/*   Updated: 2024/07/20 00:47:02 by athonda          ###   ########.fr       */
+/*   Updated: 2024/07/20 14:41:56 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ int	check_dup(long *nbrs, int count, int argc, char **str)
 		j = i + 1;
 		while (j < count)
 		{
-			if (nbrs[i] == nbrs[j] || nbrs[i] > 2147483647)
+			if (nbrs[i] == nbrs[j] || nbrs[i] > 2147483647 || nbrs[i] < -2147483648)
 			{
 				write (2, "Error\n", 6);
 				free_array_l(nbrs);
@@ -58,6 +58,26 @@ int	check_dup(long *nbrs, int count, int argc, char **str)
 				return (1);
 			}
 			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	check_int(long *nbrs, int count, int argc, char **str)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		if (nbrs[i] < -2147483648 || nbrs[i] > 2147483647)
+		{
+			write (2, "Error\n", 6);
+			free_array_l(nbrs);
+			if (argc == 2)
+				free_split(str);
+			return (1);
 		}
 		i++;
 	}
@@ -101,6 +121,8 @@ int	check_arg(char **str, int argc)
 	}
 	tmp[i] = 0;
 	if (check_dup(tmp, count, argc, str))
+		return (1);
+	if (check_int(tmp, count, argc, str))
 		return (1);
 	if (check_sort(tmp, count, argc, str))
 		return (1);
